db4d4793f79dab16422b169a9835def26e2ba7ab
Stephen Hoffman <stephen.f.hoffman@gmail.com>
2019-03-11 09:44:49 -0500

    Add CiphertextMessage class

--- ps6.py
+++ ps6.py
@@ -1,16 +1,16 @@
 import string
 
-### DO NOT MODIFY THIS FUNCTION ###
+
 def load_words(file_name):
-    '''
-    file_name (string): the name of the file containing 
-    the list of words to load    
-    
+    """
+    file_name (string): the name of the file containing
+    the list of words to load
+
     Returns: a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
-    '''
+    """
     print('Loading word list from file...')
     # inFile: file
     in_file = open(file_name, 'r')
@@ -22,15 +22,15 @@ def load_words(file_name):
     in_file.close()
     return word_list
 
-### DO NOT MODIFY THIS FUNCTION ###
+
 def is_word(word_list, word):
-    '''
+    """
     Determines if word is a valid word, ignoring
     capitalization and punctuation
 
     word_list (list): list of words in the dictionary.
     word (string): a possible word.
-    
+
     Returns: True if word is in word_list, False otherwise
 
     Example:
@@ -38,12 +38,12 @@ def is_word(word_list, word):
     True
     >>> is_word(word_list, 'asdf') returns
     False
-    '''
+    """
     word = word.lower()
     word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
     return word in word_list
 
-### DO NOT MODIFY THIS FUNCTION ###
+
 def get_story_string():
     """
     Returns: a joke in encrypted text.
@@ -53,7 +53,9 @@ def get_story_string():
     f.close()
     return story
 
+
 WORDLIST_FILENAME = 'words.txt'
+
 
 class Message(object):
     ### DO NOT MODIFY THIS METHOD ###
@@ -129,6 +131,7 @@ class Message(object):
                 text = text + shift_dict[char]
         return text
 
+
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
         """
@@ -162,41 +165,42 @@ class PlaintextMessage(Message):
         self.encrypting_dict = self.build_shift_dict(shift)
         self.message_text_encrypted = self.apply_shift(shift)
 
+
 class CiphertextMessage(Message):
     def __init__(self, text):
-        '''
+        """
         Initializes a CiphertextMessage object
-                
+
         text (string): the message's text
 
         a CiphertextMessage object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
-        '''
+        """
         pass  # delete this line and replace with your code here
 
     def decrypt_message(self):
-        '''
+        """
         Decrypt self.message_text by trying every possible shift value
         and find the "best" one. We will define "best" as the shift that
         creates the maximum number of real words when we use apply_shift(shift)
         on the message text. If s is the original shift value used to encrypt
-        the message, then we would expect 26 - s to be the best shift value 
+        the message, then we would expect 26 - s to be the best shift value
         for decrypting it.
 
-        Note: if multiple shifts are  equally good such that they all create 
+        Note: if multiple shifts are  equally good such that they all create
         the maximum number of you may choose any of those shifts (and their
         corresponding decrypted messages) to return
 
         Returns: a tuple of the best shift value used to decrypt the message
         and the decrypted message text using that shift value
-        '''
+        """
         pass  # delete this line and replace with your code here
 
 # Example test case (PlaintextMessage)
-plaintext = PlaintextMessage('hello', 2)
-print('Expected Output: jgnnq')
-print('Actual Output:', plaintext.get_message_text_encrypted())
+# plaintext = PlaintextMessage('hello', 2)
+# print('Expected Output: jgnnq')
+# print('Actual Output:', plaintext.get_message_text_encrypted())
 
 # Example test case (CiphertextMessage)
 # ciphertext = CiphertextMessage('jgnnq')
