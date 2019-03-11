7efb6d1e6b1e7fda58413a8e1a19b2e5d5aa753d
Stephen Hoffman <stephen.f.hoffman@gmail.com>
2019-03-11 11:19:54 -0500

    Minor fixes

--- ps6.py
+++ ps6.py
@@ -7,16 +7,10 @@ def load_words(file_name):
     the list of words to load
 
     Returns: a list of valid words. Words are strings of lowercase letters.
-
-    Depending on the size of the word list, this function may
-    take a while to finish.
     """
     print('Loading word list from file...')
-    # inFile: file
     in_file = open(file_name, 'r')
-    # line: string
     line = in_file.readline()
-    # word_list: list of strings
     word_list = line.split()
     print('  ', len(word_list), 'words loaded.')
     in_file.close()
@@ -30,14 +24,6 @@ def is_word(word_list, word):
 
     word_list (list): list of words in the dictionary.
     word (string): a possible word.
-
-    Returns: True if word is in word_list, False otherwise
-
-    Example:
-    >>> is_word(word_list, 'bat') returns
-    True
-    >>> is_word(word_list, 'asdf') returns
-    False
     """
     word = word.lower()
     word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
@@ -58,7 +44,6 @@ WORDLIST_FILENAME = 'words.txt'
 
 
 class Message(object):
-    ### DO NOT MODIFY THIS METHOD ###
     def __init__(self, text):
         """
         text (string): the message's text
@@ -66,38 +51,32 @@ class Message(object):
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
 
-    ### DO NOT MODIFY THIS METHOD ###
     def get_message_text(self):
-        '''
+        """
         Used to safely access self.message_text outside of the class
-        
+
         Returns: self.message_text
-        '''
+        """
         return self.message_text
 
-    ### DO NOT MODIFY THIS METHOD ###
     def get_valid_words(self):
-        '''
+        """
         Used to safely access a copy of self.valid_words outside of the class
-        
+
         Returns: a COPY of self.valid_words
-        '''
+        """
         return self.valid_words[:]
 
     def build_shift_dict(self, shift):
-        '''
+        """
         Creates a dictionary that can be used to apply a cipher to a letter.
-        The dictionary maps every uppercase and lowercase letter to a
-        character shifted down the alphabet by the input shift. The dictionary
-        should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
-        
-        shift (integer): the amount by which to shift every letter of the 
+
+        shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
 
-        Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
-        '''
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
+        """
         lower_dict = {}
         upper_dict = {}
         lower = string.ascii_lowercase * 2
@@ -113,8 +92,6 @@ class Message(object):
     def apply_shift(self, shift):
         """
         Applies the Caesar Cipher to self.message_text with the input shift.
-        Creates a new string that is self.message_text shifted down the
-        alphabet by some number of characters determined by the input shift
 
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
@@ -172,10 +149,6 @@ class CiphertextMessage(Message):
         Initializes a CiphertextMessage object
 
         text (string): the message's text
-
-        a CiphertextMessage object has two attributes:
-            self.message_text (string, determined by input text)
-            self.valid_words (list, determined using helper function load_words)
         """
         Message.__init__(self, text)
         self.valid_words = load_words(WORDLIST_FILENAME)
@@ -183,15 +156,7 @@ class CiphertextMessage(Message):
     def decrypt_message(self):
         """
         Decrypt self.message_text by trying every possible shift value
-        and find the "best" one. We will define "best" as the shift that
-        creates the maximum number of real words when we use apply_shift(shift)
-        on the message text. If s is the original shift value used to encrypt
-        the message, then we would expect 26 - s to be the best shift value
-        for decrypting it.
-
-        Note: if multiple shifts are  equally good such that they all create
-        the maximum number of you may choose any of those shifts (and their
-        corresponding decrypted messages) to return
+        and find the "best" one.
 
         Returns: a tuple of the best shift value used to decrypt the message
         and the decrypted message text using that shift value
@@ -206,13 +171,13 @@ class CiphertextMessage(Message):
             if valid_words > best_shift[2]:
                 best_shift = [i, self.apply_shift(i), valid_words]
 
-        return (best_shift[0], best_shift[1])
+        return best_shift[0], best_shift[1]
 
 
 # Example test case (PlaintextMessage)
-# plaintext = PlaintextMessage('hello', 2)
-# print('Expected Output: jgnnq')
-# print('Actual Output:', plaintext.get_message_text_encrypted())
+plaintext = PlaintextMessage('hello', 2)
+print('Expected Output: jgnnq')
+print('Actual Output:', plaintext.get_message_text_encrypted())
 
 # Example test case (CiphertextMessage)
 ciphertext = CiphertextMessage('jgnnq')
